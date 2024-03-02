385b12469fbf057015550f0b0ab61a1938091e5a
VVVV BBBB <Volodymyr>
2024-03-02 16:46:03 +0200

    added bird

--- lib/components/bird.dart
+++ lib/components/bird.dart
@@ -1,5 +1,26 @@
-enum BirdMovement {
-  middle,
-  up,
-  down,
+import 'package:bird_game_app/game/assets.dart';
+import 'package:bird_game_app/game/bird_movement.dart';
+import 'package:bird_game_app/game/flappy_bird_game.dart';
+import 'package:flame/components.dart';
+
+class Bird extends SpriteGroupComponent<BirdMovement>
+    with HasGameRef<FlappyBirdGame> {
+  Bird();
+
+  @override
+  Future<void> onLoad() async {
+    final birdMidFlap = await gameRef.loadSprite(Assets.birdMidFlap);
+    final birdUpFlap = await gameRef.loadSprite(Assets.birdUpFlap);
+    final birdDownFlap = await gameRef.loadSprite(Assets.birdDownFlap);
+
+    size = Vector2(50, 40);
+    // поточний стан для руху птаха
+    current = BirdMovement.middle;
+    position = Vector2(50, 50);
+    sprites = {
+      BirdMovement.middle: birdMidFlap,
+      BirdMovement.up: birdUpFlap,
+      BirdMovement.down: birdDownFlap,
+    };
+  }
 }
--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,14 +1,17 @@
 import 'package:bird_game_app/components/background.dart';
+import 'package:bird_game_app/components/bird.dart';
 import 'package:bird_game_app/components/ground.dart';
 import 'package:flame/game.dart';
 
 class FlappyBirdGame extends FlameGame {
+  // створимо экземпляр класу bird
+  late Bird bird;
   @override
   Future<void> onLoad() async {
     addAll([
       Background(),
       Ground(),
+      bird = Bird(),
     ]);
-
   }
 }
