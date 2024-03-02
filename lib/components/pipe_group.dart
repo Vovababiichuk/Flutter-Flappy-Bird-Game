9c39363e503389a19054b27f4289b891d57b4436
VVVV BBBB <Volodymyr>
2024-03-02 17:44:31 +0200

    added pipes logis

--- lib/components/pipe_group.dart
+++ lib/components/pipe_group.dart
@@ -1,20 +1,29 @@
+import 'dart:math';
+
 import 'package:bird_game_app/game/configuration.dart';
 import 'package:bird_game_app/game/flappy_bird_game.dart';
 import 'package:bird_game_app/game/pipe_position.dart';
 import 'package:flame/components.dart';
+import 'package:flutter/foundation.dart';
 
 import 'pipe.dart';
 
 class PipeGroup extends PositionComponent with HasGameRef<FlappyBirdGame> {
   PipeGroup();
 
+  final _random = Random();
+
   @override
   Future<void> onLoad() async {
     position.x = gameRef.size.x;
 
+    final heightMinusGround = gameRef.size.y - Config.groundHeight;
+    final spacing = 100 + _random.nextDouble() * (heightMinusGround / 4);
+    final centerY = spacing + _random.nextDouble() * (heightMinusGround - spacing);
+
     addAll([
-      Pipe(pipePosition: PipePosition.top, height: 100),
-      Pipe(pipePosition: PipePosition.bottom, height: 200),
+      Pipe(pipePosition: PipePosition.top, height: centerY - spacing / 2 ),
+      Pipe(pipePosition: PipePosition.bottom, height: heightMinusGround - (centerY + spacing / 2)),
     ]);
   }
 
@@ -22,5 +31,11 @@ class PipeGroup extends PositionComponent with HasGameRef<FlappyBirdGame> {
   void update(double dt) {
     super.update(dt);
     position.x -= Config.gameSpeed * dt;
+
+    // if (position.x < -20) {
+    if (position.x < -40) {
+      removeFromParent();
+      // debugPrint('Pipe removed');
+    }
   }
 }
