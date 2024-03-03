1119408f9f92e36160fbf3c4ffed0b39f4934178
VVVV BBBB <Volodymyr>
2024-03-03 14:16:06 +0200

    added mainMenu screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:bird_game_app/game/flappy_bird_game.dart';
+import 'package:bird_game_app/screens/main_menu_screen.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   final game = FlappyBirdGame();
   runApp(
-    GameWidget(game: game,),
+    GameWidget(game: game,
+    initialActiveOverlays: const [MainMenuScreen.id],
+    overlayBuilderMap: {
+      'mainMenu': (context, _) => MainMenuScreen(game: game),
+    },
+    ),
   );
 }
---
+++ lib/screens/main_menu_screen.dart
@@ -0,0 +1,31 @@
+import 'package:bird_game_app/game/assets.dart';
+import 'package:bird_game_app/game/flappy_bird_game.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MainMenuScreen extends StatelessWidget {
+  // створимо єеземпляр...
+  final FlappyBirdGame game;
+
+  static const String id = 'mainMenu';
+
+  const MainMenuScreen({Key? key, required this.game}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    game.pauseEngine();
+
+    return Scaffold(
+      body:  Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(Assets.menu),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
